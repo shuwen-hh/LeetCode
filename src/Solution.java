14ba907e662f849c450bb28558bd642469babbf0
shuwen <1461011514@qq>
2023-10-20 10:46:47 +0800

    测试分支

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,13 @@
 
+
 public class Solution {
 
     /**
      * 11. 盛最多的水
+     *
      * @param height 整数数组
      * @return result 容器可以储存的最大水量
-     *
+     * <p>
      * 短板移动可能会变大或者不变，长板移动肯定会变小，所以只需要移动短板，不会错过最大值
      */
     public int maxArea(int[] height) {
@@ -18,10 +20,20 @@ public class Solution {
             result = Math.max(low * (right - left), result);
             if (height[left] > height[right]) {
                 right--;
-            } else  {
+            } else {
                 left++;
             }
         }
+        return result;
+    }
+
+    /**
+     * 739. 每日温度
+     *
+     * 暴力双循环超时。。。
+     */
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
         return result;
     }
 }
